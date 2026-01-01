cda19f6d03b8eabd04ea858ba914507a8a4e5ea7
Mackintosh Odika <odikamackintosh@gmail.com>
2026-01-01 16:38:52 +0100

    db updated

--- db.js
+++ db.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render Postgres
+  },
 });
 
 pool.connect()
-    .then(() => console.log("PostgreSQL connected successfully"))
-    .catch(err => console.error("PostgreSQL connection error:", err));
+  .then(() => {
+    console.log("PostgreSQL connected successfully");
+  })
+  .catch((err) => {
+    console.error("PostgreSQL connection error:", err);
+  });
 
 module.exports = pool;
