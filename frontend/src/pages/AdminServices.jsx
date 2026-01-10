9aa1d10001ee3036fd4a5f7763cfb10ff9de542a
Mackintosh Odika <odikamackintosh@gmail.com>
2026-01-10 01:21:50 +0100

    admin services added

--- frontend/src/pages/AdminServices.jsx
+++ frontend/src/pages/AdminServices.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "../../api/axios";
+import api from "../api/axios";
 import PageWrapper from "../../components/PageWrapper";
 
 function AdminServices() {
